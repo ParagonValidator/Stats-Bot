667e14a33fa56ba9c128dfb61f8a66d86357cd77
bguillaumat <bastien.guillaumat@gmail.com>
2024-04-17 22:44:43 +0100

    Adding the actual state of the bot

---
+++ README.md
@@ -0,0 +1,58 @@
+```
+# Validator Stats Bot
+
+This JavaScript bot is designed to have some stats and infos for your validator. Follow the steps below to get started:
+
+## Installation
+
+First, ensure you have Node.js installed on your machine. You can download it [here](https://nodejs.org/).
+
+Next, clone this repository to your local machine:
+
+```bash
+git clone https://github.com/ParagonValidator/Stats-Bot.git
+```
+
+Navigate to the project directory:
+
+```bash
+cd Stats-Bot
+```
+
+Install the necessary dependencies using either npm or yarn:
+
+```bash
+npm install
+```
+or
+```bash
+yarn
+```
+
+## Configuration
+
+Before running the bot, you need to set up your environment variables. Copy the `.env.example` file and rename it to `.env`. Fill in the required values for the environment variables listed in the file.
+The telegram token can be generated using [BotFather](https://github.com/ParagonValidator/Stats-Bot.git)
+
+## Running the Bot
+
+Once you have installed the dependencies and configured the environment variables, you can start the bot by running:
+
+```bash
+node bot.js
+```
+
+The bot will now be up and running, performing its designated tasks.
+
+## Usage
+
+Once this script is running you can add your bot to your Telegram chat then calling `/start` will make it print its commands.
+
+## Contributing
+
+If you'd like to contribute to this project, feel free to fork the repository and submit a pull request with your changes. Any contributions are welcome!
+
+## Issues
+
+If you encounter any issues with the bot, please open an issue on the GitHub repository. Be sure to provide detailed information about the problem you're experiencing.
+```
---
+++ bot.js
@@ -0,0 +1,239 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fetch = require('node-fetch');
+const {Connection} = require('@solana/web3.js');
+require('dotenv').config();
+
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+const slotTimeAvgSeconds = 0.450
+const autoDeleteTimer = 60_000
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, process.env.VALIDATOR_NAME ? `Welcome ${process.env.VALIDATOR_NAME} Operators` : "Welcome Operators", {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Stake Changes",
+                        callback_data: "stakeChanges"
+                    },
+                    {
+                        text: "Ranking",
+                        callback_data: "ranking"
+                    },
+                    {
+                        text: "Epoch Infos",
+                        callback_data: "epochInfos"
+                    },
+                    {
+                        text: "Rewards",
+                        callback_data: "rewards"
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+bot.on('callback_query', async (query) => {
+    const chatId = query.message.chat.id;
+    const command = query.data;
+
+    if (command === "rewards") {
+        try {
+            const connection = new Connection(process.env.RPC_URL)
+            const leaderSchedule = await connection.getLeaderSchedule()
+            const epochInfo = await connection.getEpochInfo()
+            const leaderSlots = leaderSchedule[process.env.IDENTITY_ADDRESS] || []
+            const baseSlot = epochInfo.absoluteSlot - epochInfo.slotIndex
+            const rewards = await Promise.all(leaderSlots.filter((slotIndex) => slotIndex < epochInfo.slotIndex).map(async (slotIndex) => {
+                const slot = baseSlot + slotIndex
+                try {
+                    const slotInfo = await connection.getBlock(slot, {
+                        rewards: true,
+                        maxSupportedTransactionVersion: 0,
+                        transactionDetails: "none"
+                    })
+                    return {slot, rewards: (slotInfo?.rewards[0]?.lamports || 0) / 10 ** 9}
+                } catch (e) {
+                    return {slot, rewards: 0}
+                }
+            }))
+            const totalRewards = rewards.reduce((curr, prev) => curr + prev.rewards, 0)
+            const message = rewards.map(item => `${item.slot}: ${item.rewards.toFixed(4)} SOL`).join('\n').concat(`\n\nTotal: ${totalRewards.toFixed(4)} SOL`)
+            bot.sendMessage(chatId, message).then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        } catch (e) {
+            console.log(e)
+            bot.sendMessage(chatId, "Error: the action couldn't be processed!").then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        }
+    }
+
+    if (command === "epochInfos") {
+        try {
+            const response = await fetch("https://api.stakewiz.com/epoch_info");
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const data = await response.json();
+            const connection = new Connection(process.env.RPC_URL)
+            const leaderSchedule = await connection.getLeaderSchedule()
+            const epochInfo = await connection.getEpochInfo()
+            const actualSlot = epochInfo.slotIndex
+            const leaderSlots = leaderSchedule[process.env.IDENTITY_ADDRESS] || []
+            const nextSlot = findSmallestGreaterThanX(leaderSlots, actualSlot)
+            bot.sendMessage(chatId, `Epoch: ${data.epoch}\nLeader Slots: ${leaderSlots?.length || 0}\nRemaining Slots: ${leaderSlots.filter(num => num > actualSlot).length}\nNext Slot in: ~${formatSeconds(parseInt((nextSlot - actualSlot) * slotTimeAvgSeconds))}\nRemaining Time: ${formatSeconds(data.remaining_seconds)}\n`).then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        } catch (e) {
+            console.log(e)
+            bot.sendMessage(chatId, "Error: the action couldn't be processed!").then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        }
+    }
+
+    if (command === "ranking") {
+        try {
+            const response = await fetch(`https://api.stakewiz.com/validator/${process.env.VOTE_ADDRESS}`);
+            if (!response.ok) {
+                throw new Error("Failed to fetch data").then((sentMessage) => {
+                    const messageId = sentMessage.message_id;
+                    setTimeout(() => {
+                        bot.deleteMessage(chatId, messageId);
+                    }, autoDeleteTimer);
+                });
+            }
+            const data = await response.json();
+            bot.sendMessage(chatId, `Rank: ${data.rank}\nWiz Score: ${data.wiz_score}%\nVote Success: ${data.vote_success}%\nSkip rate: ${data.skip_rate}%\n`).then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        } catch (e) {
+            console.log(e)
+            bot.sendMessage(chatId, "Error: the action couldn't be processed!").then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        }
+    }
+
+    if (command === "stakeChanges") {
+        try {
+            const response = await fetch(`https://api.stakewiz.com/validator_epoch_stake_accounts/${process.env.VOTE_ADDRESS}`);
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const data = await response.json();
+
+            let activating = data.activating.stake_accounts.map((obj) => {
+                return {
+                    amount: parseFloat(obj.delegated_amount),
+                    formatted: parseFloat(obj.delegated_amount).toFixed(2) + " SOL"
+                };
+            });
+            activating.sort((a, b) => b.amount - a.amount);
+            let totalActivating = activating.reduce((acc, cur) => acc + cur.amount, 0);
+
+            let deactivating = data.deactivating.stake_accounts.map((obj) => {
+                return {
+                    amount: parseFloat(obj.delegated_amount),
+                    formatted: parseFloat(obj.delegated_amount).toFixed(2) + " SOL"
+                };
+            });
+            deactivating.sort((a, b) => b.amount - a.amount);
+            let totalDeactivating = deactivating.reduce((acc, cur) => acc + cur.amount, 0);
+
+            let message = "";
+            if (activating.length > 0) {
+                const formattedActivating = "Activating:\n" + activating.map(entry => `- ${entry.formatted}`).join("\n");
+                message += formattedActivating + "\nTotal Activating: " + totalActivating.toFixed(2) + " SOL\n\n";
+            }
+            if (deactivating.length > 0) {
+                const formattedDeactivating = "Deactivating:\n" + deactivating.map(entry => `- ${entry.formatted}`).join("\n");
+                message += formattedDeactivating + "\nTotal Deactivating: " + totalDeactivating.toFixed(2) + " SOL\n";
+            }
+
+            if (message.trim() !== "") {
+                bot.sendMessage(chatId, message).then((sentMessage) => {
+                    const messageId = sentMessage.message_id;
+                    setTimeout(() => {
+                        bot.deleteMessage(chatId, messageId);
+                    }, autoDeleteTimer);
+                });
+            } else {
+                bot.sendMessage(chatId, "No stake data available.").then((sentMessage) => {
+                    const messageId = sentMessage.message_id;
+                    setTimeout(() => {
+                        bot.deleteMessage(chatId, messageId);
+                    }, autoDeleteTimer);
+                });
+            }
+        } catch (e) {
+            console.log(e);
+            bot.sendMessage(chatId, "Error: the action couldn't be processed!").then((sentMessage) => {
+                const messageId = sentMessage.message_id;
+                setTimeout(() => {
+                    bot.deleteMessage(chatId, messageId);
+                }, autoDeleteTimer);
+            });
+        }
+    }
+});
+
+function formatSeconds(seconds) {
+    var days = Math.floor(seconds / (3600 * 24));
+    var hours = Math.floor((seconds % (3600 * 24)) / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var secondsLeft = seconds % 60;
+
+    var result = "";
+    if (days > 0) {
+        result += days + "d ";
+    }
+    if (hours > 0) {
+        result += hours + "h ";
+    }
+    if (minutes > 0) {
+        result += minutes + "m ";
+    }
+    if (secondsLeft > 0) {
+        result += secondsLeft + "s";
+    }
+
+    return result.trim();
+}
+
+function findSmallestGreaterThanX(array, x) {
+    // Filter the array to get only numbers greater than X
+    const filteredArray = array.filter(num => num > x);
+
+    // If there are no numbers greater than X, return null
+    if (filteredArray.length === 0) {
+        return null;
+    }
+
+    // Find the smallest number greater than X
+    return Math.min(...filteredArray);
+}
